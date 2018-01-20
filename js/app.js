825b144ae5c905f8adb001c3bde33a1a71a1a505
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2018-01-20 10:10:53 -0300

    add props

--- js/app.js
+++ js/app.js
@@ -1,52 +1,62 @@
 // APP
-var INCHES = 25.4;
-
-var canvas = {
+var config = {
+	INCHES: 25.4,
 	resolution: 150,
 	widthMM: 60,
-	heightMM: 95
-};
+	heightMM: 95,
+	getSize: config => {
+		return {
+			width: config.resolution * (config.widthMM / config.INCHES),
+			height: config.resolution * (config.heightMM / config.INCHES),
+		}
+	}
+}
 
 ////////////////////////////////////////
 
-var width = canvas.resolution * (canvas.widthMM / INCHES),
-	height = canvas.resolution * (canvas.heightMM / INCHES),
+var pageSize = config.getSize(config),
 	page = new Konva.Stage({
 		container: 'container',
-		width: width,
-		height: height
+		width: pageSize.width,
+		height: pageSize.height
 	});
 
 var cardGrid = new Konva.Layer();
 
-var {renderCard,cards,images} = Deck;
+var Deck = getDeck(config);
+var {
+	renderCard,
+	cards,
+	imageUrls
+} = Deck;
 
 var imagesCount = 0,
 	imagesLoaded = 0;
-for(var a in images){
+for (var a in imageUrls) {
 	imagesCount++;
 	var imageObj = new Image();
 	imageObj.onload = () => {
 		imagesLoaded++;
 	};
 	imageObj.onerror = () => {
-		console.log('ERROR. La imagen "'+ a +'" no se pudo cargar.');
+		console.log('ERROR. La imagen "' + a + '" no se pudo cargar.');
 	};
 	var r = +new Date;
-	imageObj.src = images[a]+'?'+r;
+	imageObj.src = imageUrls[a] + '?' + r;
+	Deck.images[a] = imageObj;
 }
 
 var renderPage = () => {
 	cards.forEach(card => {
-		var cardToRender =  renderCard(card);
+		var cardToRender = renderCard(card);
 		cardGrid.add(cardToRender);
 	});
 	page.add(cardGrid);
 }
 
-var imagesWait = setInterval(function(){
-	if(imagesLoaded === imagesCount){
+var imagesWait = setInterval(function() {
+	if (imagesLoaded === imagesCount) {
 		clearInterval(imagesWait);
 		renderPage();
 	}
-},100);
+}, 100);
--- js/deck.js
+++ js/deck.js
@@ -1,25 +1,58 @@
-var Deck = {
-	cardSize: {
-		widthMM: 56,
-		heightMM: 89
-	},
-	images:{
-		yoda:'images/yoda.jpg'
-	},
-	renderCard(card) {
-		var cardToRender = new Konva.Rect({
-			x: 10,
-			y: 10,
-			width: 100,
-			height: 50,
-			fill: card.color,
-			stroke: 'black',
-			strokeWidth: 4
-		});
+var getDeck = config => {
 
-		return cardToRender;
-	},
-	cards: [{
-		color: 'yellow'
-	}]
+	var cardConfig = Object.assign({}, config, {
+			widthMM: 56,
+			heightMM: 89
+		}),
+		imageUrls = {
+			yoda: 'images/yoda.jpg'
+		};
+
+	var RED = 'RED',
+		YELLOW = 'YELLOW',
+		ORANGE = 'ORANGE',
+		GREEN = 'GREEN',
+		BLUE = 'BLUE',
+		BLACK = 'BLACK';
+
+	var S = 'S',
+		A = 'A',
+		I = 'I',
+		II = 'II',
+		III = 'III',
+		IV = 'IV',
+		V = 'V';
+
+
+	var Deck = {
+		cardSize: config.getSize(cardConfig),
+		imageUrls,
+		images: {},
+		renderCard(card) {
+
+			var {
+				images
+			} = Deck;
+
+
+			var cardToRender = new Konva.Image({
+				x: 50,
+				y: 50,
+				image: images.yoda,
+				width: 106,
+				height: 118
+			});
+
+			return cardToRender;
+		},
+		cards: [{
+			color: RED
+			age: I,
+			name: 'Templo',
+			icons:[],
+			image: 'yoda'
+		}]
+	};
+
+	return Deck;
 };
